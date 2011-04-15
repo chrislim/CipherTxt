e46006fc58d3c8d9ae0fed3a98fc3ff30fd310ec
Chris Lim <chrislim@uw.edu>
2011-04-15 00:45:31 -0700

    adding basic gesture recognition

--- CipherTxt/src/com/joshuahou/ciphertxt/CipherTxtMain.java
+++ CipherTxt/src/com/joshuahou/ciphertxt/CipherTxtMain.java
@@ -3,17 +3,106 @@ package com.joshuahou.ciphertxt;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.ClipboardManager;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class CipherTxtMain extends Activity {
+	
+    private float mPosX;
+    private float mPosY;
+    
+    private float mLastTouchX;
+    private float mLastTouchY;
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+        
         final EditText editText = (EditText) findViewById(R.id.message);
+        editText.setOnTouchListener(new View.OnTouchListener() {
+        	private static final int INVALID_POINTER_ID = -1;
+
+        	// The ‘active pointer’ is the one currently moving our object.
+        	private int mActivePointerId = INVALID_POINTER_ID;
+
+        	// Existing code ...
+
+        	@Override
+        	public boolean onTouch(View v, MotionEvent ev) {
+        	    final int action = ev.getAction();
+        	    switch (action & MotionEvent.ACTION_MASK) {
+        	    case MotionEvent.ACTION_DOWN: {
+        	        final float x = ev.getX();
+        	        final float y = ev.getY();
+        	        
+        	        mLastTouchX = x;
+        	        mLastTouchY = y;
+
+        	        // Save the ID of this pointer
+        	        mActivePointerId = ev.getPointerId(0);
+        	        break;
+        	    }
+        	        
+        	    case MotionEvent.ACTION_MOVE: {
+        	        // Find the index of the active pointer and fetch its position
+        	        final int pointerIndex = ev.findPointerIndex(mActivePointerId);
+        	        final float x = ev.getX(pointerIndex);
+        	        final float y = ev.getY(pointerIndex);
+        	        
+        	        final float dx = x - mLastTouchX;
+        	        final float dy = y - mLastTouchY;
+        	        
+        	        mPosX += dx;
+        	        mPosY += dy;
+        	        
+        	        if(dx > 0){
+        	        	String password = ((EditText) findViewById(R.id.password)).getText().toString();
+                        editText.setText(encrypt(password, editText.getText().toString()));
+        	        } else if(dx < 0){
+        	        	String password = ((EditText) findViewById(R.id.password)).getText().toString();
+                        editText.setText(decrypt(password, editText.getText().toString()));
+        	        }
+        	        
+        	        mLastTouchX = x;
+        	        mLastTouchY = y;
+        	        
+        	        break;
+        	    }
+        	        
+        	    case MotionEvent.ACTION_UP: {
+        	        mActivePointerId = INVALID_POINTER_ID;
+        	        break;
+        	    }
+        	        
+        	    case MotionEvent.ACTION_CANCEL: {
+        	        mActivePointerId = INVALID_POINTER_ID;
+        	        break;
+        	    }
+        	    
+        	    case MotionEvent.ACTION_POINTER_UP: {
+        	        // Extract the index of the pointer that left the touch sensor
+        	        final int pointerIndex = (action & MotionEvent.ACTION_POINTER_ID_MASK) 
+        	                >> MotionEvent.ACTION_POINTER_ID_SHIFT;
+        	        final int pointerId = ev.getPointerId(pointerIndex);
+        	        if (pointerId == mActivePointerId) {
+        	            // This was our active pointer going up. Choose a new
+        	            // active pointer and adjust accordingly.
+        	            final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+        	            mLastTouchX = ev.getX(newPointerIndex);
+        	            mLastTouchY = ev.getY(newPointerIndex);
+        	            mActivePointerId = ev.getPointerId(newPointerIndex);
+        	        }
+        	        break;
+        	    }
+        	    }
+        	    
+        	    return true;
+        	}
+		});
+        
         final Button encryptButton = (Button) findViewById(R.id.encryptbutton);
         encryptButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
